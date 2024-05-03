765ddd9dff50c4fbd2686fa0b9576e785735f5b0
Akshay Mugale <akshaymugale1@gmail.com>
2024-05-03 18:48:14 +0530

    IP change

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { getItemInLocalStorage } from "../utils/localStorage";
+
+const axiosInstance = axios.create({
+  baseURL: "http://13.215.74.38",
+});
+
+axiosInstance.interceptors.request.use(
+  (authenticate) => {
+    const token = getItemInLocalStorage("TOKEN");
+    if (token) {
+      authenticate.headers["Authorization"] = ` ${token}`;
+    }
+    return authenticate;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
