fdca789091eba783aa07e5e2eef3b1ea3458a88a
Kunal sah <sahkunal94@gmail.com>
2024-05-26 18:58:22 +0530

    new brach

--- src/Buttons/BackButton.jsx
+++ src/Buttons/BackButton.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 import { IoMdArrowBack } from "react-icons/io";
 import { NavLink } from "react-router-dom";
-const BackButton = ({to}) => {
+const BackButton = ({ to }) => {
   return (
     <div>
       <ul className="p-2 bg-black rounded-xl flex max-w-screen w-fit mx-1 text-white text-md text-center justify-between flex-wrap gap-2 ">
-          <NavLink
-            to={to}
-            className={({ isActive }) =>
-              ` ${
-                isActive
-                  ? "text-black bg-white p-2 font-medium rounded-md item-start  text-sm "
-                  : "  items-center text-sm  font-medium p-2 hover:bg-white hover:text-black rounded-md "
-              }`
-            }
-          >
-            <IoMdArrowBack />
-          </NavLink>
-        </ul>
+        <NavLink
+          to={to}
+          className={({ isActive }) =>
+            ` ${
+              isActive
+                ? "text-black bg-white p-2 font-medium rounded-md item-start  text-sm "
+                : "  items-center text-sm  font-medium p-2 hover:bg-white hover:text-black rounded-md "
+            }`
+          }
+        >
+          <IoMdArrowBack />
+        </NavLink>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default BackButton
+export default BackButton;
+// check
